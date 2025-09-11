00d264626b143431ce06eb9fcfd3f4bacb9bc0ca
FreZkaa <asmysikov@gmail.com>
2025-09-11 13:14:34 +0300

    Add files via upload

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,14 @@
             bool answer = false;
 
             // code here
-
+            //var input = new double[] { 0, 1.5, 1, 3, -1, -2.3, 0.78, -0.3 };
+            // var answer = new bool[] { false, true, true, true, true, true, false, false };
+            double n = double.Parse(Console.ReadLine());
+            if (Math.Abs(n) >= 1.0)
+            {
+                Console.WriteLine("true");
+            }
+            else { Console.WriteLine("false"); }
             // end
 
             return answer;
@@ -17,7 +24,16 @@
             bool answer = false;
 
             // code here
-
+            //var input = new double[] { 0, 1.5, 1, 3, -1, -2.3, 0.78, -0.3 };
+            // var answer = new bool[] { false, true, true, true, true, true, false, false };
+            double n = double.Parse(Console.ReadLine());
+            double t =  double.Parse(Console.ReadLine());
+            double summ = n+t;
+            if (summ > 0.0)
+            {
+                Console.WriteLine("true");
+            }
+            else { Console.WriteLine("false"); }
             // end
 
             return answer;
@@ -27,7 +43,16 @@
             bool answer = false;
 
             // code here
-
+            //var input = new int[] { 0, 5, 2, -3, -2, 11, 22, -31, -418 };
+            //var answer = new bool[] { true, true, false, false, true, true, false, false, false };
+            int n = int.Parse(Console.ReadLine());
+            int t =  int.Parse(Console.ReadLine());
+            double summ = n+t;
+            if (summ > ((Math.Abs(n) + Math.Abs(t)) / 2))
+            {
+                Console.WriteLine("true");
+            }
+            else { Console.WriteLine("false"); }
             // end
 
             return answer;
@@ -37,7 +62,14 @@
             int answer = 0;
 
             // code here
-
+            //var input = new int[] { 0, 5, 2, -3, -2, 11, 22, -31, -418 };
+            // var answer = new int[] { 5, 5, 2, 11, 22, 22, 22, 0, 5 };
+            int n = int.Parse(Console.ReadLine());
+            int t = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int maxx = n > t ? n : t;
+            maxx = maxx > p ? maxx : p;
+            Console.WriteLine(maxx);
             // end
 
             return answer;
@@ -47,7 +79,18 @@
             double answer = 0;
 
             // code here
-
+            //var input = new double[] { 0, 1.5, 1, 0.3, -1, -2.3, 0.78, -0.3 };
+            // var answer = new double[] { -1, 0, 0, -0.91, 0, 0, -0.39159999, -0.91 };
+            double n = double.Parse(Console.ReadLine());
+            double y;
+            if (Math.Abs(n) > 1)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(n * n - 1);
+            }
             // end
 
             return answer;
@@ -57,7 +100,22 @@
             bool answer = false;
 
             // code here
-
+            //var input = new double[,] {
+            //    { 0, 1.5, 1, 1, -1, 0.3, -1, -0.3, 0.78, -0.3 },
+            //    { 0, 2.3, -1, 1, 1, 0.1, 1.23, -0.2, 0.41, 0.69 }
+            //};
+            //var answer = new bool[] { true, false, false, false, false, true, false, false, false }; ;
+            double n = double.Parse(Console.ReadLine());
+            double t = double.Parse(Console.ReadLine());
+            if (t >= 0)
+            {
+                if (n < 0 && t <= 1 + n)
+                    Console.WriteLine("true");
+                else if (n >= 0 && t <= 1 - n)
+                    Console.WriteLine("true");
+                else { Console.WriteLine("false"); }
+            }
+            else { Console.WriteLine("false"); }
             // end
 
             return answer;
@@ -68,7 +126,23 @@
             bool answer = true;
 
             // code here
-
+            //var input = new int[] { 0, 5, 2, -3, -2, 11, 22, -31, -418 };
+            // var answer = new bool[] { false, true, false, false, false, true, false, false, false };
+            bool f = true;
+            int t = int.Parse(Console.ReadLine());
+            if (t < 0)
+            {
+                f = false;
+            }
+            else
+            {
+                if (t % 2 == 0)
+                {
+                    f = false;
+                }
+                else { f = true; }
+            }
+            Console.WriteLine(f);
             // end
 
             return answer;
@@ -76,9 +150,33 @@
         public bool Task8(int X, int Y)
         {
             bool answer = false;
-
-            // code here
-
+            //code here
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int bed = 4 * 60;   
+            int wake = 14 * 60;   
+            for (int day = 1; day <= x; day++)
+            {
+                wake -= 60;
+                if (day % 2 == 1)
+                {
+                    bed -= y;
+                }
+                if (bed < 0) bed += 24 * 60;
+                if (wake < 0) wake += 24 * 60;
+                if (wake == 7 * 60)
+                {
+                    int sleep = wake - bed;
+                    if (sleep < 0) sleep += 24 * 60; 
+                    double h = sleep / 60.0;
+                    if (h >= 7 && h <= 9)
+                    {
+                        Console.WriteLine("true");
+                        System.Threading.Thread.CurrentThread.Abort();
+                    }
+                }
+            }
+            Console.WriteLine("false");
             // end
 
             return answer;
